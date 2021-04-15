bd5fea24f6160edcf3ad74cacb5a6bb8e7d3302f
Rob <robsc801@gmail.com>
2021-04-15 15:10:06 +1200

    pull in products json to products seed

--- server/db/seeds/1_products.js
+++ server/db/seeds/1_products.js
@@ -1,36 +1,36 @@
-const csv = require('csv-parser')
-const fs = require('fs')
-const path = require('path')
-const results = [];
+// const csv = require('csv-parser')
+// const fs = require('fs')
+// const path = require('path')
+// const results = [];
 
-fs.createReadStream(path.join(__dirname, 'sample_products.csv'))
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    const products = results.filter(p => {
-      return p.Type == 'simple'
-    }).map(p => {
-      let id = Number(Object.values(p)[0])
-        return {
-          title: p.Name, 
-          description: p.Description,
-          image: p.Images,
-        }
-    })
+// fs.createReadStream(path.join(__dirname, 'sample_products.csv'))
+//   .pipe(csv())
+//   .on('data', (data) => results.push(data))
+//   .on('end', () => {
+//     const products = results.filter(p => {
+//       return p.Type == 'simple'
+//     }).map(p => {
+//       let id = Number(Object.values(p)[0])
+//         return {
+//           title: p.Name, 
+//           description: p.Description,
+//           image: p.Images,
+//         }
+//     })
 
-    fs.writeFile(path.join(__dirname, 'products.json'), JSON.stringify(products, null, 4), () => { //null, 4 will indent ewvery level by 4 spaces
-console.log('all done')  
-    })  
+//     fs.writeFile(path.join(__dirname, 'products.json'), JSON.stringify(products, null, 4), () => { //null, 4 will indent ewvery level by 4 spaces
+// console.log('all done')  
+//     })  
 
-    //console.log(products);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
-  })
+//     //console.log(products);
+//     // [
+//     //   { NAME: 'Daffy Duck', AGE: '24' },
+//     //   { NAME: 'Bugs Bunny', AGE: '22' }
+//     // ]
+//   })
 
 
-
+const products = require('./products.json')
 
 exports.seed = (knex) => {
   // Deletes ALL existing entries
