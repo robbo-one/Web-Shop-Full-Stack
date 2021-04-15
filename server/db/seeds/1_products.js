7f050ad405fcb2fa8ba7ef1deaee98729b60d47a
Rob <robsc801@gmail.com>
2021-04-15 14:39:00 +1200

    add sample products to db &  install parser

--- server/db/seeds/1_products.js
+++ server/db/seeds/1_products.js
@@ -1,3 +1,21 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const path = require('path')
+const results = [];
+
+fs.createReadStream(path.join(__dirname, 'sample_products.csv'))
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+    // [
+    //   { NAME: 'Daffy Duck', AGE: '24' },
+    //   { NAME: 'Bugs Bunny', AGE: '22' }
+    // ]
+  })
+
+
+
 
 exports.seed = (knex) => {
   // Deletes ALL existing entries
