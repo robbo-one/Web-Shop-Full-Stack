b418327544b90bcebdf1184002c368ddceb75479
Rob <robsc801@gmail.com>
2021-04-21 12:15:57 +1200

    App component rendering products

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+const imagePlaceholderUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNW-SjEM9fr4dVJ6KaPzOYL_OMA0obZxa_MA&usqp=CAU'
 
 class App extends React.Component {
     // For React Only have state in App and componentDidMount -> setState({})
@@ -10,13 +13,40 @@ class App extends React.Component {
     componentDidMount() {
         // fetchProducts()
     }
+
     render() {
+        // const products = this.state.products //React only
+        const products = this.props.products
+        
         return (
             <>
             <h1>Hi there!</h1>
+            <ul>
+                {products.map (p => {
+                    return (
+                        <li key={p.id}>
+                        <h3>{p.title}</h3>
+                        <img src={p.image || imagePlaceholderUrl } style={{ maxWidth:  '200px'}} />
+                        </li>
+                    )
+                 }) }
+            </ul>
             </>
         )
     }
 }
 
-export default App
+//Fancy pants way
+// function mapStateToProps({products}) {
+//     return {
+//         products,
+//     }
+// }
+
+function mapStateToProps(globalState) {
+    return {
+        products: globalState.products,
+    }
+}
+
+export default connect(mapStateToProps)(App)
--- client/reducers/products.js
+++ client/reducers/products.js
@@ -1,4 +1,15 @@
-const initialState = []
+const initialState = [
+{
+    id: 1,
+    title: 'Product 1',
+    image: '',
+},
+{
+    id: 1,
+    title: 'Product b',
+    image: '',
+},
+]
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
